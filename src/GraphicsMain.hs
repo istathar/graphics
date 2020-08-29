7f81096d6f6c203bd6a41be7ba589b502634a84e
Andrew Cowie <istathar@gmail.com>
2020-08-29 22:55:49 +1000

    Format with ormolu

--- src/GraphicsMain.hs
+++ src/GraphicsMain.hs
@@ -6,14 +6,14 @@ import Graphics.UI.Gtk
 
 main :: IO ()
 main = do
-    initGUI
-    button <- buttonNewWithLabel "Press!"
-    window <- windowNew
-    containerAdd window button
-    widgetShowAll window
-    on window objectDestroy mainQuit
+  initGUI
+  button <- buttonNewWithLabel "Press!"
+  window <- windowNew
+  containerAdd window button
+  widgetShowAll window
+  on window objectDestroy mainQuit
 
-    on button buttonActivated $ do
-        putStrLn "Clicked!"
+  on button buttonActivated $ do
+    putStrLn "Clicked!"
 
-    mainGUI
+  mainGUI
