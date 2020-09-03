d7df27cd5946d5cc51840ea29adaab8831112925
Andrew Cowie <istathar@gmail.com>
2020-09-03 21:52:39 +1000

    Change to using gi-gtk

--- src/GraphicsMain.hs
+++ src/GraphicsMain.hs
@@ -1,19 +1,33 @@
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
 {-# OPTIONS_GHC -fno-warn-unused-imports #-}
 
-module Main where
-
-import Graphics.UI.Gtk
+import Data.GI.Base
+import qualified GI.Gtk as Gtk
 
 main :: IO ()
 main = do
-  initGUI
-  button <- buttonNewWithLabel "Press!"
-  window <- windowNew
-  containerAdd window button
-  widgetShowAll window
-  on window objectDestroy mainQuit
+  Gtk.init Nothing
 
-  on button buttonActivated $ do
-    putStrLn "Clicked!"
+  window <- new Gtk.Window [#title := "Hi there"]
 
-  mainGUI
+  on window #destroy Gtk.mainQuit
+
+  button <- new Gtk.Button [#label := "Click me"]
+
+  on
+    button
+    #clicked
+    ( set
+        button
+        [ #sensitive := False,
+          #label := "Thanks for clicking me"
+        ]
+    )
+
+  #add window button
+
+  #showAll window
+
+  Gtk.main
